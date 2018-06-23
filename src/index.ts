155c7e26ac384046faf401d1ad6eb29e989849b3
Paul Betts <paul@paulbetts.org>
2018-06-22 17:43:48 -0700

    Let's leave this off for now

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { Action } from './action';
+// export { Action } from './action';
 export { createCollection } from './custom-operators';
 export { ChangeNotification, TypedChangeNotification, Model, notify, fromObservable, WhenSelector } from './model';
-export { MergeStrategy, Updatable } from './updatable';
+// export { MergeStrategy, Updatable } from './updatable';
 export { getValue, when, whenProperty } from './when';
