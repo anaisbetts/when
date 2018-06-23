aead3f34abfbd50ffab5effc5fc1e122dbdead9c
Paul Betts <paul@paulbetts.org>
2018-06-22 17:20:36 -0700

    Fix publishing packages

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Action } from './action';
+export { createCollection } from './custom-operators';
+export { ChangeNotification, TypedChangeNotification, Model, notify, fromObservable, WhenSelector } from './model';
+export { MergeStrategy, Updatable } from './updatable';
+export { getValue, when, whenProperty } from './when';
