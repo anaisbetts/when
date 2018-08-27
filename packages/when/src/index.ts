aed16b3ea742c91a57d4bdcb2742b37f356fe043
Paul Betts <paul@paulbetts.org>
2018-08-26 18:51:04 -0700

    Add updatable

--- packages/when/src/index.ts
+++ packages/when/src/index.ts
@@ -1,5 +1,5 @@
 // export { Action } from './action';
 export { createCollection } from './custom-operators';
 export { ChangeNotification, TypedChangeNotification, Model, notify, fromObservable } from './model';
-// export { MergeStrategy, Updatable } from './updatable';
+export { MergeStrategy, Updatable } from './updatable';
 export { getValue, when, whenProperty } from './when';
